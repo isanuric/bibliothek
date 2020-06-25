5d314a068cfdbf632a2ca00dada5accf5e96186a
Ehsan Salmani <ehsan.salmani@ic-consult.de>
2020-06-25 23:30:05 +0200

    Update pom

--- src/test/java/com/bib/BibliothekApplicationTests.java
+++ src/test/java/com/bib/BibliothekApplicationTests.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+@ContextConfiguration(classes = BibliothekApplication.class)
 @RunWith(SpringRunner.class)
 @AutoConfigureWebTestClient
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+//@TestConfiguration
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BibliothekApplicationTests {
 
     @Autowired
